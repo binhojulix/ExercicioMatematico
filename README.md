e5b115a7bba0151d8a2dfda007bfc59247a5d8be
binhojulix <fabio.jdluz@gmail.com>
2021-10-21 22:34:45 -0300

    commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ExericioMatematico
+
+# para criar a imagem
+docker build -t  exerciciomatematico:v1 .
+
+# para subir a imagem
+docker run -it --rm --name exerciciomatematico exerciciomatematico:v1
---
+++ source/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calculadora.h"
+
+int main(){
+    double a=1;
+    double b=22;
+    double resultado = somar(a, b);
+    printf("o resultado é %lf", resultado);
+     printf("Commit 1\n");
+    printf("Escolha o nível de dificuldade\n");
+    printf("1 Números entre 0-9\n");
+    printf("2 Números entre 10-99\n1"); 
+    printf("3 Números entre 100-999\n1");
+    printf("4 Números entre 1000-9999\n1"); 
+    printf("5 Números entre 10000-99999\n1");
+    printf("6 Números entre 100000-9999999\n1"); 
+    printf("7 Números entre 1000000-99999999\n");
+    
+}
